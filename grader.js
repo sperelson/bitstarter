3f98e616f0123ba7334769c7a8e27efc0eaa1592
Stephen Perelson <sperelson@gmail.com>
2013-07-07 16:13:50 +0200

    grader.js can now use --url

--- grader.js
+++ grader.js
@@ -24,7 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var restler = require('restler');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -37,8 +37,23 @@ var assertFileExists = function(infile) {
 	return instr;
 };
 
+var doOutput = function(out) {
+	var outJson = JSON.stringify(out, null, 4);
+	console.log(outJson);
+}
+
 var cheerioHtmlFile = function(htmlfile) {
 	return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioURL = function(url) {
+	var data2;
+	rest.get(url).on('complete', function(data) {
+		data2 = data;
+	console.log(data2);
+	});
+	console.log(data2);
+	return cheerio.load(data2);
 };
 
 var loadChecks = function(checksfile) {
@@ -56,6 +71,19 @@ var checkHtmlFile = function(htmlfile, checksfile) {
 	return out;
 };
 
+var checkURL = function(url, checksfile) {
+	rest.get(url).on('complete', function(data) {
+		$ = cheerio.load(data);
+		var checks = loadChecks(checksfile).sort();
+		var out = {};
+		for(var ii in checks) {
+			var present = $(checks[ii]).length > 0;
+			out[checks[ii]] = present;
+		}
+		doOutput(out);
+	});
+};
+
 var clone = function(fn) {
 	// Workaround for commander.js issue.
 	// http://stackoverflow.com/a/6772648
@@ -66,10 +94,14 @@ if(require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <html_file>', 'URL to index.html')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+	if (program.url === false) {
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		doOutput(checkJson);
+	} else {
+		var checkJson = checkURL(program.url, program.checks);
+	}
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
